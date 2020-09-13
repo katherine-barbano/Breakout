708306d39fe5c655c2e97d3007d8ffe9763e0cf8
Katherine Barbano <katherine.barbano@duke.edu>
2020-09-13 03:07:10 -0400

    Screen appears from Racer lab

--- src/breakout/Main.java
+++ src/breakout/Main.java
@@ -1,13 +1,44 @@
 package breakout;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+
 /***
  * Calls launch(args) to start the program.
  */
-public class Main {
+public class Main extends Application{
+
+    public static final String TITLE = "Racer JavaFX";
+    public static final int SIZE = 400;
+    public static final int FRAMES_PER_SECOND = 60;
+    public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
+    public static final Paint BACKGROUND = Color.AZURE;
+
+    // some things needed to remember during game
+    private Scene myScene;
+
+
     /**
      * Start of the program.
      */
     public static void main (String[] args) {
-        System.out.println("Hello world");
+        launch(args);
+    }
+
+    @Override
+    public void start (Stage stage) {
+        Game game = new Game(stage);
     }
 }
