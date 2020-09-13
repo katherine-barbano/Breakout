9d85f62cd11dc53246aaa8cf813c14bae91e1607
Katherine Barbano <katherine.barbano@duke.edu>
2020-09-13 02:23:20 -0400

    Added some initial classes for Test and setup JUnit testing and TestFX

---
+++ test/breakout/MainTest.java
@@ -0,0 +1,19 @@
+package breakout;
+
+import util.DukeApplicationTest;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ * Tests for Game class.
+ *
+ * @author Robert C Duvall
+ */
+public class MainTest extends DukeApplicationTest {
+
+  @Test
+  public void start () {
+
+  }
+}
