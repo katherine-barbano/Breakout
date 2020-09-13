e62bedc8772011579797b8412811d8d3e31e91a6
Katherine Barbano <katherine.barbano@duke.edu>
2020-09-13 18:17:48 -0400

    Drew Paddle and Ball on screen

--- src/breakout/Ball.java
+++ src/breakout/Ball.java
@@ -1,5 +1,9 @@
 package breakout;
 
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+
 /***
  * Purpose: A circle that breaks Blocks, and ends the game if it crosses the
  * bottom of the screen. Bounces off all other walls of the screen, as well as
@@ -7,6 +11,14 @@ package breakout;
  *
  * Method: isOnScreen, breakBlock, isTouchingBlock, isTouchingPaddle
  */
-public class Ball {
+public class Ball extends Circle {
 
+  public static final Paint BALL_COLOR = Color.RED;
+  public static final int BALL_RADIUS = 15;
+
+  public Ball(int sceneWidth, Paddle paddle) {
+    super(sceneWidth/2, paddle.getY() - BALL_RADIUS, BALL_RADIUS);
+    setFill(BALL_COLOR);
+    setId("ball");
+  }
 }
